2e32e4d47d29e68ba902651c690094e701e199fb
Relja <relja.gajic@gmail.com>
2026-09-19 17:21:11 +0200

    Refactor: group directional light and flashlight state into structs

--- app/include/app/MainController.hpp
+++ app/include/app/MainController.hpp
@@ -12,15 +12,31 @@ public:
     void on_mouse_move(engine::platform::MousePosition position) override;
 };
 
+struct DirLight {///STRUKTURA DIRLIGHT UBACENA KAO I SVE ODGOVARAJUCE PROMENE U CPP I GUI CPP
+    glm::vec3 ambient;
+    glm::vec3 diffuse;
+    glm::vec3 direction;
+};
+
+struct Flashlight {//STRUKTURA FLASHLIGHT UBACENA, KAO I SVE ODGOVARAJUCE PROMENE U CPP I GUI CPP
+    enum class FlashlightState {
+        Off,
+        Ukljucivanje,
+        Treperi,
+        On
+    };
+    FlashlightState stanje{FlashlightState::Off};
+    float timer{0.0f};
+};
+
+
 class MainController final : public engine::core::Controller {
 public:
     std::string_view name() const override {
         return "app::MainController";
     }
 
-    glm::vec3 &dir_light_ambient() { return m_dir_light_ambient; }
-    glm::vec3 &dir_light_diffuse() { return m_dir_light_diffuse; }
-    glm::vec3 &dir_light_direction() { return m_dir_light_direction; }
+    DirLight &dir_light() { return m_dir_light; }//GETTER PROMENJEN TKD POKAZUJE NA STRUKTURU
 
 private:
     void initialize() override;
@@ -47,22 +63,16 @@ private:
 
     bool m_cursor_enabled{true};
 
-    glm::vec3 m_dir_light_ambient{0.05f, 0.05f, 0.15f};
-    glm::vec3 m_dir_light_diffuse{0.2f, 0.2f, 0.4f};
-    glm::vec3 m_dir_light_direction{-0.8f, -1.0f, -0.3f};
+    DirLight m_dir_light{
+            .ambient = {0.05f, 0.05f, 0.15f},
+            .diffuse = {0.2f, 0.2f, 0.4f},
+            .direction = {-0.8f, -1.0f, -0.3f}};
 
-    enum class FlashlightState {
-        Off,
-        Ukljucivanje,
-        Treperi,
-        On
-    };
+    Flashlight m_baterijska;
 
     void update_baterijska();
-    float baterijska_snaga() const;
 
-    FlashlightState m_baterijska_stanje{FlashlightState::Off};
-    float m_baterijska_timer{0.0f};
+    float baterijska_snaga() const;
 };
 }// namespace app
 #endif//MAINCONTROLLER_HPP
